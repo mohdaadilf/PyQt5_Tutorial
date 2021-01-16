9ccb96f1538393696cfb13824ee400750ef9d5bd
Mohd Aadil Faizal <mohdaadilf@gmail.com>
2021-01-17 03:45:09 +0530

    2nd Commit

--- Menus and toolbars.py
+++ Menus and toolbars.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QAction, qApp
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QAction, QMenu, QTextEdit
 from PyQt5.QtGui import QIcon
 
 
@@ -16,16 +16,41 @@ class Example(QMainWindow):
         self.setWindowTitle('Status bar')
         self.show()
 
+        self.statusbar = self.statusBar()
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
+        fileMenu = menubar.addMenu('&File')  # creating menu
 
-        exitAct = QAction('&Exit', self)
+        exitAct = QAction(QIcon('hdds.ico'), 'Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(self.close)
 
-        fileMenu.addAction(exitAct)
+        impmenu = QMenu('Import', self)  # sub menu
+        impAct = QAction('Import mail', self)
+        impmenu.addAction(impAct)
 
+        viewStatAct = QAction('View Statusbar', self, checkable=True)
+        viewStatAct.setStatusTip('View statusbar')
+        viewStatAct.setChecked(True)
+        viewStatAct.triggered.connect(self.toggleMenu)
+
+        fileMenu.addAction(exitAct)
+        fileMenu.addAction(viewStatAct)
+        fileMenu.addMenu(impmenu)
+
+        self.toolbar = self.addToolBar('')
+        self.toolbar.addAction(exitAct)
+        self.toolbar.addAction(viewStatAct)
+
+        textEdit = QTextEdit()
+        self.setCentralWidget(textEdit)
+
+    def toggleMenu(self, state):
+        if state:
+            self.statusbar.show()
+            self.statusbar.showMessage("On")
+        else:
+            self.statusbar.hide()
 
     def closeEvent(self, event):
         reply = QMessageBox.question(self, 'Message', "Are you sure to quit?", QMessageBox.Yes | QMessageBox.No,
@@ -35,6 +60,17 @@ class Example(QMainWindow):
             event.accept()
         else:
             event.ignore()
+
+    def contextMenuEvent(self, event):
+        cmenu = QMenu(self)
+
+        newAct = cmenu.addAction("New")
+        openAct = cmenu.addAction("Open")
+        quitAct = cmenu.addAction("Quit")
+        action = cmenu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == quitAct:
+            self.close()
 
 def main():
     app = QApplication(sys.argv)
